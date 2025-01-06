402d44f12dda8ae7c9b3d00e910897cd0ccc9b8f
hsg5533 <hsg5533@naver.com>
2025-01-06 21:33:03 +0900

    fix: mysql infomation

--- modules/mysql.js
+++ modules/mysql.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2"),
     host: "localhost",
     port: "3306",
     user: "root",
-    password: "1234",
+    password: "",
     database: "market",
   };
 module.exports = {
